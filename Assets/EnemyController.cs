fc29e728333da9af028dd9bcb0b0e7fb9acf2fc6
keithhopkins <knhopkins412@gmail.com>
2018-02-25 21:42:44 -0500

    First person movement with non-moving sword and a couple buildings

---
+++ Assets/EnemyController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+	public float health;
+
+	// Use this for initialization
+	void Start () {
+		health = 100;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (health <= 0) {
+			Dead();
+		}
+	}
+
+	void ApplyDamage (int damage) {
+		health -= damage;
+	}
+
+	void Dead() {
+		Destroy (gameObject);
+	}
+}
---
+++ Assets/Melee.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Melee : MonoBehaviour {
+	
+	public int Damage = 50;
+	public float Distance;
+	public float Range = 1.5f; 
+	public string message;
+
+	public void Update() {
+		if (Input.GetButtonDown("Fire1")) {
+			RaycastHit hit;
+			if (Input.GetButton("SwingLeft") && !Input.GetButton("SwingRight")) {
+				HorizontalSwingLeft();
+			} else if (Input.GetButton("SwingRight") && !Input.GetButton("SwingLeft")) {
+				HorizontalSwingRight();
+			}
+			if (Physics.Raycast (transform.position, transform.TransformDirection (Vector3.forward), out hit)) {
+				Distance = hit.distance;
+				if (Distance < Range) {
+					hit.transform.SendMessage ("ApplyDamage", Damage, SendMessageOptions.DontRequireReceiver);
+				}
+			}
+		}
+	}
+
+	void HorizontalSwingLeft() {
+		message = "SwingLeft";
+	}
+
+	void HorizontalSwingRight() {
+		message = "SwingRight";
+	}
+}
